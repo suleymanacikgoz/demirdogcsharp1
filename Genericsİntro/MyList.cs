21d19967844843504b41c238d836d4ecf93bc586
suleymanacikgoz <62662812+suleymanacikgoz@users.noreply.github.com>
2021-01-20 23:53:39 +0300

    20 ocak ders sonu

---
+++ Genericsİntro/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genericsİntro
+{
+
+
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] TempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                items[i] = TempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+
+
+        
+    }
+}
---
+++ Genericsİntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Süleyman");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
